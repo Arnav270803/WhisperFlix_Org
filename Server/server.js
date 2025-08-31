5d48ed7dbd6e89d0e1ecefa3a82457fd9f848c9f
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-08-31 20:47:05 +0530

    Setting up server.js

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const PORT = process.env.port || 4000
+const app = express()
+
+app.use(express.json) // all the request will be pass using json method 
+app.use(cors) // connect backend and frontend
+
+app.get((req,res) => (
+    req.send("API Working ") // this will sends a request that API is working 
+))
+
+app.listen(PORT, () =>
+    console.log('Server running on port '+ PORT) 
+)
