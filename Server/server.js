f64e1928ab648e11461432a9a129a69bded0ecfb
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-09-02 23:28:54 +0530

    added mongodb gatabase and connected it with express

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
 
 const PORT = process.env.port || 4000
 const app = express()
 
 app.use(express.json) // all the request will be pass using json method 
 app.use(cors) // connect backend and frontend
+await connectDB()// connecting the mongodb database
 
 app.get((req,res) => (
     req.send("API Working ") // this will sends a request that API is working 
